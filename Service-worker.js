6d3d8311697a9d87dd898b3a61abb6b66d653ecb
vanhmingmuana-ctrl <vanhmingmuana@gmail.com>
2026-05-18 20:44:57 +0530

    Add files via upload

---
+++ Service-worker.js
@@ -0,0 +1,4 @@
+Service-worker.js
+self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {});
