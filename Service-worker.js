a5a3bcf36eff25305da2bb9dc3d45b2fd6277af9
vanhmingmuana-ctrl <vanhmingmuana@gmail.com>
2026-05-18 22:40:37 +0530

    Update Service-worker.js

--- Service-worker.js
+++ Service-worker.js
@@ -1,4 +1,8 @@
-Service-worker.js
-self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {});
+const CACHE_NAME = 'candy-crush-v1';
+const urlsToCache = ['./', './index.html', './Manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
